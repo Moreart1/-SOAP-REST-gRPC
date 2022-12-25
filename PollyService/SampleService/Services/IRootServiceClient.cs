977f8e48476992835d84a327b3a6e18fb25687db
Moreart1 <Morearti1726263@yandex.ru>
2022-12-25 15:02:16 +0300

    Урок 6. Введение в проектирование и отказоустойчивость

---
+++ PollyService/SampleService/Services/IRootServiceClient.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleService.Services
+{
+    public interface IRootServiceClient
+    {
+        public RootServiceNamespace.RootServiceClient Client { get; }
+
+        public Task<ICollection<RootServiceNamespace.WeatherForecast>> Get();
+    }
+}
